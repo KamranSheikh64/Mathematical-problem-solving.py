b8a748c8766825918857936c8599e235b93ad0bf
KamranSheikh64 <136167698+KamranSheikh64@users.noreply.github.com>
2023-06-15 13:30:30 +0530

    Add files via upload

---
+++ Circles problem.py
@@ -0,0 +1,17 @@
+# call any of them and write radius value between brackets
+#like:diameter(2),circumfrance(3),area_of_circle(4)
+def diameter(radius):
+    print(2*radius)
+
+def circumfrance(radius):
+    print(2*3.14*radius)
+
+def area_of_circle(radius):
+    print(3.14*radius**2)
+
+diameter(2)
+
+
+
+
+
